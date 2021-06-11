5db4e7903cdbeecacb909c7b163bcc6c54a41ed2
anna dodo <anna.dorenskaya@gmail.com>
2021-06-11 12:28:53 +0200

    deleting

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -18,4 +18,10 @@ export class TasksComponent implements OnInit {
     })
    }
 
+   deleteTask(task: Task) {
+    this.TaskService.deleteTask(task).subscribe(() => {
+      this.tasks = this.tasks.filter(t => t.id !== task.id)
+    })
+   }
+
 }
