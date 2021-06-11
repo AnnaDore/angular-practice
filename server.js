e65a5867b3da5ec29b2336f4ea46dc98e208818f
anna dodo <anna.dorenskaya@gmail.com>
2021-06-11 14:10:43 +0200

    fake-server4

--- server.js
+++ server.js
@@ -34,20 +34,19 @@
 
 // server.listen(port);
 
+
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('/db.json'); 
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
-  static: './build'
+  //static: './build'
 });
-const port = process.env.PORT || 4200;
-
+const PORT = process.env.PORT || 4200;
 server.use(middlewares);
 server.use(jsonServer.rewriter({
-  '/api/*': '/$1'
+  '/api/*': '/$1',
 }))
 server.use(router);
-
 server.listen(PORT, () => {
-  console.log("Server is running")
+  console.log('Server is running');
 });
